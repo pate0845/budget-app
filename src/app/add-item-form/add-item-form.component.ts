e70e72945aa86049f7dc01e52b50b4c8cbfe7231
pate0845 <pate0845@algonquinlive.com>
2021-05-03 22:58:10 -0500

    budget-ui

---
+++ src/app/add-item-form/add-item-form.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+
+@Component({
+  selector: 'app-add-item-form',
+  templateUrl: './add-item-form.component.html',
+  styleUrls: ['./add-item-form.component.scss']
+})
+export class AddItemFormComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+  onSubmit(form: NgForm) {
+    console.log(form);
+  }
+
+}
